815d8fbb6af203ea9d35254eeea7bfc303f81754
Alex Paige <apaige161@gmail.com>
2020-12-08 21:07:05 -0500

    added increment logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hellong';
+  title = 'hello world';
+  counter: number = 0;
+  name: string = 'Alex'
+
+  number: number = 0
+
+  increaseCounter(add: number) {
+    this.counter = this.counter + add;
+  }
+
+  decreaseCounter(subtract: number) {
+    if(this.counter > 0) {
+      this.counter = this.counter - subtract;
+    }
+  }
+
+  resetCounter() {
+    this.counter = 0;
+  }
+  
+  
+
 }
+
+
